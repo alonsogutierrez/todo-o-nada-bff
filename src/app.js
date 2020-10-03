92faa25b8611213fec4cc621dbf650442da9eb0d
Alonso Gutierrez <vn0jbpg@M-C02Z901ALVCG.local>
2020-10-03 18:50:38 -0300

    feat(health): add health routes

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const cors = require('cors');
 require('./db/mongoose');
 const orderRouter = require('./api/orders/routes/order');
+const healthRouter = require('./api/health/routes/health');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(orderRouter);
+app.use(healthRouter);
 
 module.exports = app;
