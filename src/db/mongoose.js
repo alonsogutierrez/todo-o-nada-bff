f546e89de38bb1b3c1631ab1b3d2aef1d0a1344b
Alonso Gutierrez <vn0jbpg@M-C02Z901ALVCG.local>
2020-10-04 18:37:29 -0300

    feat(console): erase console logs

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,5 @@
 const mongoose = require('mongoose');
 
-console.log('MONGODB_URL: ', process.env.MONGODB_URL);
-console.log('MONGO_DB_USER: ', process.env.MONGO_DB_USER);
-console.log('MONGO_DB_PASSWORD: ', process.env.MONGO_DB_PASSWORD);
-
 mongoose.connect(process.env.MONGODB_URL, {
   user: process.env.MONGO_DB_USER,
   pass: process.env.MONGO_DB_PASSWORD,
