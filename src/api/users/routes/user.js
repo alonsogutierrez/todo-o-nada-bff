00899e6fece1ed26a7b0cff398415e61f240a248
Alonso Gutierrez <vn0jbpg@M-C02Z901ALVCG.local>
2020-11-03 10:43:30 -0300

    feat(login): add logs in login flow, change status 400 to 404 when user cant login

--- src/api/users/routes/user.js
+++ src/api/users/routes/user.js
@@ -6,32 +6,29 @@ const logger = console;
 
 router.post('/users', async (req, res) => {
   const user = new User(req.body);
-  logger.log('User: ', user);
   try {
-    logger.log('Going to save user');
     await user.save();
     logger.log('User saved succesfully');
-    logger.log('Generating token authentication');
     const token = await user.generateAuthToken();
     res.status(201).send({ user, token });
   } catch (e) {
     logger.error('Can`t save user: ', e.message);
-    res.status(400).send(e.message);
+    res.status(500).send(e.message);
   }
 });
 
 router.post('/users/login', async (req, res) => {
   try {
-    console.log('req.body: ', req.body);
     const user = await User.findByCredentials(
       req.body.email,
       req.body.password
     );
-    console.log('user: ', user);
     const token = await user.generateAuthToken();
+    logger.log('User succesfully login');
     res.send({ user, token });
   } catch (e) {
-    res.status(400).send(e);
+    logger.error('Cant login user');
+    res.status(404).send(e.message);
   }
 });
 
