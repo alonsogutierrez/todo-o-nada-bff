cfceba9f1db21db226ca8646601bb36747037fad
Cristian Gutiérrez <instanciageek@gmail.com>
2021-01-21 23:14:56 -0300

    Add routes to: - [GET] products - [GET] products/:sku - [PUT] products/sku

--- src/api/products/routes/product.js
+++ src/api/products/routes/product.js
@@ -16,4 +16,40 @@ router.post('/product', async (req, res) => {
     }
 })
 
+router.get('/product', async (req, res) => {
+    try {
+        const products = await Product.find({})
+        logger.log('Products get successfully');
+        res.status(201).send({ status: true , data: products })
+    } catch (e) {
+        logger.error('Can`t gets product: ', e.message);
+        res.status(500).send({status: false, error: e.message});
+    }
+})
+
+router.get('/product/:sku', async (req, res) => {
+    try {
+        const { sku } = req.params
+        const product = await Product.findOne({sku: parseInt(sku)})
+        logger.log(`Product with sku :${sku} find successfully`);
+        res.status(201).send({ status: true , data: product })
+    } catch (e) {
+        logger.error('Can`t gets product: ', e.message);
+        res.status(500).send({status: false, error: e.message});
+    }
+})
+
+router.put('/product/:sku', async (req, res) => {
+    try {
+        const { sku } = req.params
+        const updateProduct = new Product(req.body);
+        const product = await Product.findOneAndUpdate({sku: parseInt(sku)}, updateProduct)
+        logger.log(`Product with sku :${sku} update successfully`);
+        res.status(201).send({ status: true , data: product })
+    } catch (e) {
+        logger.error('Can`t gets product: ', e.message);
+        res.status(500).send({status: false, error: e.message});
+    }
+})
+
 module.exports = router;
--- src/db/models/product.js
+++ src/db/models/product.js
@@ -87,6 +87,4 @@ const productSchema = new mongoose.Schema(
   }
 );
 
-const Product = mongoose.model('Product', productSchema);
-
-module.exports = Product;
+module.exports = mongoose.model('Product', productSchema);
