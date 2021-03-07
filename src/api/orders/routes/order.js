477b40d02c7df7c45617beb8ce92eca4b09bf69e
vn0jbpg <alonso.gutierrez@walmart.com>
2021-03-07 19:07:44 -0300

    feat(searchOrder): create new endpoint to get an order by orderNumber, add uuid package

--- src/api/orders/routes/order.js
+++ src/api/orders/routes/order.js
@@ -6,17 +6,44 @@ const router = new express.Router();
 const logger = console;
 
 router.post('/orders', async (req, res) => {
-  //TODO: Create an orderNumber
-  const orderNumber = await idBuilder.generateOrderId();
-  req.body.orderNumber = orderNumber;
-  const order = new Order(req.body);
-  logger.log('Order: ', order);
   try {
+    const orderNumber = await idBuilder.generateOrderId();
+    req.body.orderNumber = orderNumber;
+    const order = new Order(req.body);
+    logger.log('Order: ', order);
     await order.save();
     logger.log('Order saved succesfully!');
     res.status(201).send({ order });
   } catch (e) {
     logger.error('Can`t save order: ', e.message);
+    res.status(500).send(e.message);
+  }
+});
+
+router.get('/orders', async (req, res) => {
+  try {
+    const query = req.query;
+    if (!query) {
+      throw new Error('Invalid request');
+    }
+    const { orderNumber } = query;
+    if (!orderNumber) {
+      throw new Error('Invalid orderNumber');
+    }
+    const orderDocs = await Order.find({
+      orderNumber: Number(orderNumber)
+    });
+    if (!orderDocs) {
+      res.status(404).send('Order not found');
+    }
+    logger.log(
+      orderDocs.length +
+        ' Orders found like orderNumber equal to ' +
+        orderNumber
+    );
+    res.status(200).send(orderDocs);
+  } catch (e) {
+    logger.error('Can`t found order: ', e.message);
     res.status(500).send(e.message);
   }
 });
