7d51b5958f3a5016326622cdb7e615e589649e9a
Alonso Gutierrez <alonso.gutierrez@mail.udp.cl>
2021-07-08 22:47:27 -0400

    feat(dispatch): add enum validation to order payment schema in order to avoid invalid dispatch values

--- src/api/orders/adapters/POSTCreateOrderPaymentSchema.js
+++ src/api/orders/adapters/POSTCreateOrderPaymentSchema.js
@@ -1,3 +1,6 @@
+const store_dispatch = 'PICKUP';
+const home_delivery = 'HOME_DELIVERY';
+
 const orderPaymentSchema = {
   title: 'OrderPaymentSchema',
   description: 'describes properties required to create an order payment',
@@ -14,6 +17,7 @@ const orderPaymentSchema = {
     dispatchData: {
       type: 'string',
       description: 'dispatch data description',
+      enum: [store_dispatch, home_delivery],
     },
   },
 };
