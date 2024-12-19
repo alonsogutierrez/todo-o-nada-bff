fd31125c375f2a01c5cd1e4bca3173b81d6e84de
alonsogutierrez <alonso.gutierrez@mail.udp.cl>
2024-12-18 22:03:14 -0300

    feat(categories): add new categories

--- src/cron/jobs.js
+++ src/cron/jobs.js
@@ -1,7 +1,7 @@
-const RemoteRestData = require('../api/shared/infrastructure/RemoteRESTData');
+const RemoteRestData = require("../api/shared/infrastructure/RemoteRESTData");
 const {
   getMoreInterestingProducts,
-} = require('../api/search/useCases/products');
+} = require("../api/search/useCases/products");
 
 const logger = console;
 
@@ -18,38 +18,39 @@ const initialLoad = async (type) => {
 
 const healthCheck = async () => {
   try {
-    logger.info('Beging Health check response');
+    logger.info("Beging Health check response");
     const healthResponse = await RemoteRestData.GETRequest(
       process.env.BASE_URL_BFF,
-      '/health'
+      "/health"
     );
-    logger.info('Health check response: ', healthResponse);
+    logger.info("Health check response: ", healthResponse);
     return healthResponse;
   } catch (err) {
-    logger.error('Health check error: ', err.message);
+    logger.error("Health check error: ", err.message);
     throw new Error(`Health check error: ${err.message}`);
   }
 };
 
 const getInterestingProducts = async () => {
   try {
-    logger.info('Begin to get interesting products to cache');
+    logger.info("Begin to get interesting products to cache");
     const promisesResponses = new Array(4).fill(null);
-    promisesResponses[0] = await getMoreInterestingProducts(0, 10, 'principal');
-    promisesResponses[1] = await getMoreInterestingProducts(0, 10, 'second');
-    promisesResponses[2] = await getMoreInterestingProducts(0, 10, 'third');
-    promisesResponses[3] = await getMoreInterestingProducts(0, 10, 'four');
+    // TODO: Get all promises in parallel
+    promisesResponses[0] = await getMoreInterestingProducts(0, 10, "principal");
+    promisesResponses[1] = await getMoreInterestingProducts(0, 10, "second");
+    promisesResponses[2] = await getMoreInterestingProducts(0, 10, "third");
+    promisesResponses[3] = await getMoreInterestingProducts(0, 10, "four");
 
-    logger.info('Promises Responses: ', promisesResponses);
+    logger.info("Promises Responses: ", promisesResponses);
 
-    interestingProductsConfig['principal'] = promisesResponses[0];
-    interestingProductsConfig['second'] = promisesResponses[1];
-    interestingProductsConfig['third'] = promisesResponses[2];
-    interestingProductsConfig['four'] = promisesResponses[3];
+    interestingProductsConfig["principal"] = promisesResponses[0];
+    interestingProductsConfig["second"] = promisesResponses[1];
+    interestingProductsConfig["third"] = promisesResponses[2];
+    interestingProductsConfig["four"] = promisesResponses[3];
 
     return interestingProductsConfig;
   } catch (err) {
-    logger.error('Cant get interesting products from cron: ', err.message);
+    logger.error("Cant get interesting products from cron: ", err.message);
   }
 };
 
